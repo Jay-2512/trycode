209a539f591a7d2711bce1e120f0bb2620037296
Arun Gopinath <agnath18K@gmail.com>
2023-11-13 16:21:12 +0530

    Fixed duplication of files & folders.
    Clean root folder on exit

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import subprocess
 import zipfile
 import os
 import time
+import uuid
+import shutil
 
 app = Flask(__name__)
 
@@ -22,20 +24,25 @@ def upload_files():
 def compile_code():
     code = request.files['code']
     testcase = request.files['testcase']
-    code.save('main.cpp')
-    testcase.save('testcases.zip')
+    code_filename = str(uuid.uuid4()) + '.cpp'
+    testcase_filename = str(uuid.uuid4()) + '.zip'
+    code.save(code_filename)
+    testcase.save(testcase_filename)
 
-    with zipfile.ZipFile('testcases.zip', 'r') as zip_ref:
-        zip_ref.extractall('testcase')
+    testcase_folder = str(uuid.uuid4())
+    os.mkdir(testcase_folder)
 
-    input_files = sorted([f for f in os.listdir('testcase') if f.startswith('input')])
-    output_files = sorted([f for f in os.listdir('testcase') if f.startswith('output')])
+    with zipfile.ZipFile(testcase_filename, 'r') as zip_ref:
+        zip_ref.extractall(testcase_folder)
+
+    input_files = sorted([f for f in os.listdir(testcase_folder) if f.startswith('input')])
+    output_files = sorted([f for f in os.listdir(testcase_folder) if f.startswith('output')])
 
     execution_times = {}
 
     for input_file, output_file in zip(input_files, output_files):
-        input_path = os.path.join('testcase', input_file)
-        output_path = os.path.join('testcase', output_file)
+        input_path = os.path.join(testcase_folder, input_file)
+        output_path = os.path.join(testcase_folder, output_file)
 
         with open(input_path, 'r') as f:
             input_data = f.readlines()  
@@ -43,7 +50,8 @@ def compile_code():
         with open(output_path, 'r') as f:
             expected_output = f.read().strip()
 
-        os.system('g++ main.cpp -o program')
+        compile_command = f'g++ -o program {code_filename}'
+        compile_output = subprocess.getoutput(f'time -f "Compilation Time: %E" {compile_command}')
 
         start_time = time.time()
         process = subprocess.run(['./program'], input='\n'.join(input_data), capture_output=True, text=True)
@@ -63,19 +71,25 @@ def compile_code():
                 'status': 'Failed'
             }
 
+    os.remove(code_filename)
+    os.remove(testcase_filename)
+    os.remove('program')
+    shutil.rmtree(testcase_folder)
+
     return render_template('result.html', execution_times=execution_times)
-
-
 
 @app.route('/compileCode', methods=['POST'])
 def compileCode():
     code = request.form['code']
-    with open('program.cpp', 'w') as file:
+    code_filename = str(uuid.uuid4()) + '.cpp'
+    with open(code_filename, 'w') as file:
         file.write(code)
 
-    compile_command = 'g++ -o program program.cpp'
+    compile_command = f'g++ -o program {code_filename}'
     compile_output = subprocess.getoutput(f'time -f "Compilation Time: %E" {compile_command}')
-    subprocess.getoutput('rm program')
+
+    os.remove(code_filename)
+    os.remove('program')
 
     return compile_output
 
